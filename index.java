e1735d64a263883eeb99f0140e170a84086ed126
lamarRoulhac123 <phantomhr6@gmail.com>
2020-11-11 02:23:56 +0000

    i finished

--- index.java
+++ index.java
@@ -1,18 +1,37 @@
 void setup() {
-	size(400, 400);
+    size(400, 400);
 }
 
 void draw() {
-	background(255);
-	stroke(0);
+    background(255);
+    stroke(0);
 
   //start creating the lines
-
+line(200, 0, 200, 400);
+line(0, 200, 400, 200);
 
   //Fill a black color
-  
+  fill(0);
 
   //Build your conditional statement 
-	
+if (mouseX <= 200 & mouseY <= 200){
+    rect(0, 0, 200, 200);
+
+}
+
+
+if (mouseX >= 200 & mouseY <= 200){
+    rect(200, 0, 200, 200);
+
+}
+
+if (mouseX <= 200 & mouseY >= 200){
+    rect(0, 200, 200, 200);
+
+}
+
+if (mouseX >= 200 & mouseY >= 200){
+    rect(200, 200, 200, 200);
+}
 
 }
